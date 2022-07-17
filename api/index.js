6162d750e176bad785f39a210d43dba71e289cfc
Hecatonquir <maxicuentas92@hotmail.com>
2022-07-17 00:54:47 -0300

    trying to make the app faster?

--- api/index.js
+++ api/index.js
@@ -7,8 +7,8 @@ const { getEpisodes } = require('./src/functions.js');
 // Para la precarga cuando se levanta el server, ejecutar la funcion getEpisodes(). Al ser una peticion vamos a usar async await.
 
 conn.sync({ force: false }).then(async () => {
-	await getEpisodes();
 	server.listen(process.env.PORT, () => {
 		console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
 	});
+	await getEpisodes();
 });
